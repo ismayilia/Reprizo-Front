84da78af9aec3139def891c19e371b78f0e688ed
HP <[ismayilia@code.edu.az]>
2023-12-29 03:53:35 +0400

    added new secions

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,5 +1,6 @@
 $(function () {
 
+ 
   // Wishlist icon Start
   let wishWhiteIcons = document.querySelectorAll(".product .product-icons .fa-regular")
 
@@ -21,5 +22,80 @@ $(function () {
     })
   });
 
+
+  // FIlter
+
+  (function () {
+
+    var parent = document.querySelector(".range-slider");
+    if (!parent) return;
+
+    var
+      rangeS = parent.querySelectorAll("input[type=range]"),
+      numberS = parent.querySelectorAll("input[type=number]");
+
+    rangeS.forEach(function (el) {
+      el.oninput = function () {
+        var slide1 = parseFloat(rangeS[0].value),
+          slide2 = parseFloat(rangeS[1].value);
+
+        if (slide1 > slide2) {
+          [slide1, slide2] = [slide2, slide1];
+
+        }
+
+        numberS[0].value = slide1;
+        numberS[1].value = slide2;
+      }
+    });
+
+    numberS.forEach(function (el) {
+      el.oninput = function () {
+        var number1 = parseFloat(numberS[0].value),
+          number2 = parseFloat(numberS[1].value);
+
+        if (number1 > number2) {
+          var tmp = number1;
+          numberS[0].value = number2;
+          numberS[1].value = tmp;
+        }
+
+        rangeS[0].value = number1;
+        rangeS[1].value = number2;
+
+      }
+    });
+
+  })();
+
+  //Sort
+  $(document).on("click", ".item0", function () {
+    let usdText = $(".item0").eq(0).text();
+    $(".button-dropdown").eq(0).text(usdText)
+  })
+
+  $(document).on("click", ".item1", function () {
+    let usdText = $(".item1").eq(0).text();
+    $(".button-dropdown").eq(0).text(usdText)
+  })
+
+  $(document).on("click", ".item2", function () {
+    let usdText = $(".item2").eq(0).text();
+    $(".button-dropdown").eq(0).text(usdText)
+  })
+
+  $(document).on("click", ".item3", function () {
+    let usdText = $(".item3").eq(0).text();
+    $(".button-dropdown").eq(0).text(usdText)
+  })
+
+  $(document).on("click", ".item4", function () {
+    let usdText = $(".item4").eq(0).text();
+    $(".button-dropdown").eq(0).text(usdText)
+  })
+
+
   // Wishlist icon End
 })
+
+
